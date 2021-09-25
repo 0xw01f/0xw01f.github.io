b24306935c53ffbcd7da5ff33d6c52c6c99875bd
W01F <76235339+0xw01f@users.noreply.github.com>
2021-09-25 23:37:02 +0200

    Update arnaques.php

--- arnaques.php
+++ arnaques.php
@@ -1,3 +1,3 @@
-<meta http-equiv="refresh" content="0; url=https://brainy-player-956.notion.site/Arnaques-1bc981598d71499e93d1053f9d2d8e76" />
+<meta http-equiv="refresh" content="0; url=https://brainy-player-956.notion.site/Arnaques-1bc981598d71499e93d1053f9d2d8e76"/>
 
 <a href="https://brainy-player-956.notion.site/Arnaques-1bc981598d71499e93d1053f9d2d8e76"> Click here if you haven't being redirected yet.</a>
